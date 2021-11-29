088826154cd0ccf6235eaf2bbc298ae2b6035502
Maggie <alis-weasley@mail.ru>
2021-11-29 14:49:57 +0300

    add Example012_Methods project

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,4 +1,5 @@
-﻿for (int i = 2; i <= 10; i++)
+﻿/*
+for (int i = 2; i <= 10; i++)
 {
     for (int j = 2; j <= 10; j++)
     {
@@ -6,3 +7,36 @@
     }
     Console.WriteLine();
 }
+*/
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винегероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s ="qwerty"
+//            012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
